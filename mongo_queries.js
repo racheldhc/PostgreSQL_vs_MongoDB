60d78f9593357ffe1f58a4466d1869572c9700cd
Rachel Callaghan <rachelcallaghan81@gmail.com>
2019-11-03 17:45:38 +0000

    Add missing reference

--- mongo_queries.js
+++ mongo_queries.js
@@ -19,6 +19,7 @@ db.movies.find(function() {return this.year > 2010 || this.year < 2000})
 // Updates
 db.movies.update({title : "The Hobbit: An Unexpected Journey"}, {$set : {synopsis : "A reluctant hobbit, Bilbo Baggins, sets out to the Lonely Mountain with a spirited group of dwarves to reclaim their mountain home - and the gold within it - from the dragon Smaug." }})
 db.movies.update({title : "The Hobbit: The Desolation of Smaug"}, {$set : {synopsis : "The dwarves, along with Bilbo Baggins and Gandalf the Grey, continue their quest to reclaim Erebor, their homeland, from Smaug. Bilbo Baggins is in possession of a mysterious and magical ring." }})
+// add to set https://docs.mongodb.com/manual/reference/operator/update/addToSet/
 db.movies.update({title : "Pulp Fiction"}, {$addToSet : {actors : "Samuel L. Jackson"}})
 
 // Text Searches
